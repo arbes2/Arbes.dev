0c18b745b0b9dbd596aa1fa7ff0ea7edff545a8b
Arbes <arbes2@gmail.com>
2022-10-12 21:36:54 +0100

    Header.js
    updated

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Design | Code | Test | Deploy
+      </SectionTitle>
+      <SectionText>
+        Hi there! I'm Arbes Istrefi<br/>
+        I bring ideas to life digitally.
+      </SectionText>
+      <Button onClick={() => window.location = 'http://google.com'}>Learn More</Button>
+    </LeftSection>
+    </Section>
 );
 
 export default Hero;
